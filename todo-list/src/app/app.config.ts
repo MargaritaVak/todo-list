f39afa3fa0e6fa01767a12ad22b810a67352b447
Margarita <MVakhrusheva@nota.tech>
2023-11-16 15:57:27 +0300

    creating an authorization form

--- todo-list/src/app/app.config.ts
+++ todo-list/src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import {importProvidersFrom} from '@angular/core';
+import { MatNativeDateModule} from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(),  importProvidersFrom(MatNativeDateModule)]
 };
--- todo-list/src/app/auth-page/auth-page.component.ts
+++ todo-list/src/app/auth-page/auth-page.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-auth-page',
   templateUrl: './auth-page.component.html',
   styleUrls: ['./auth-page.component.scss'],
   standalone: true,
+  imports: [MatFormFieldModule, 
+    MatInputModule, 
+    MatCheckboxModule,
+    MatButtonModule, 
+    MatDividerModule,],
 })
 export class AuthPageComponent implements OnInit {
 
