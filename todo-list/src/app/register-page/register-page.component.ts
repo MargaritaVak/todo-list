8c6ccd69c80ec308f4d98a28ee15839ff79a2427
Margarita <MVakhrusheva@nota.tech>
2023-11-17 13:43:49 +0300

    add reactive forms in register

--- todo-list/src/app/register-page/register-page.component.ts
+++ todo-list/src/app/register-page/register-page.component.ts
@@ -5,7 +5,10 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { AuthorizationService } from '../services/authorization.service';
 
 @Component({
   selector: 'app-register-page',
@@ -18,13 +21,37 @@ import { RouterLink } from '@angular/router';
     RouterLink,
     MatButtonModule, 
     MatDividerModule,
-    MatListModule,],
+    MatListModule,
+    ReactiveFormsModule,
+    CommonModule],
 })
 export class RegisterPageComponent implements OnInit {
+  registrationForm: FormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthorizationService) {
+    this.registrationForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      login: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });}
 
   ngOnInit() {
+    this.authService.getUsers().subscribe(
+      (res) => {
+        console.log('Полученные пользователи:', res);
+      }
+    );    
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      const userData = this.registrationForm.value;
+      console.log(userData);
+      this.authService.registerUser(userData)
+          console.log('Пользователь успешно зарегистрирован:', userData);
+    } else {
+      console.log('Форма заполнена некорректно. Пожалуйста, проверьте данные.');
+    }
   }
 
 }
--- todo-list/src/app/services/authorization.service.ts
+++ todo-list/src/app/services/authorization.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizationService {
+  private usersSubject: BehaviorSubject<any[]>;
 
-constructor() { }
+constructor() { 
+   const savedUsers = (typeof localStorage !== 'undefined') ? localStorage.getItem('users') : null;
+   this.usersSubject = new BehaviorSubject<any[]>(savedUsers ? JSON.parse(savedUsers) : []);
+}
 
+
+registerUser(user: any): void{
+ 
+    const users = this.usersSubject.getValue();
+    users.push(user);
+    this.usersSubject.next(users);
+    localStorage.setItem('users', JSON.stringify(users));
+}
+
+getUsers(): Observable<any[]> {
+  return this.usersSubject.asObservable();
+
+}
 }
