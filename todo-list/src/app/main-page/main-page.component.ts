e621856010b40af2611ffbdeaeb67242ccbf6823
Margarita <MVakhrusheva@nota.tech>
2023-11-21 11:08:46 +0300

    Сreated task record sheet

--- todo-list/src/app/main-page/main-page.component.ts
+++ todo-list/src/app/main-page/main-page.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
+  standalone: true,
+  imports: [
+    MatTableModule,
+    MatToolbarModule,
+    MatButtonModule
+  ]
 })
 export class MainPageComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'theme', 'description', 'author', 'date_creation', 'date_completed', 'changes'];
 
   constructor() { }
 
